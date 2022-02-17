5b0cf7dfff699ddcc01079c9d4cfa81184a12ee8
JRSmiffy <jamesrichardsmith97@gmail.com>
2022-02-17 15:35:14 +0000

    solid-oop initial commit

---
+++ solid-oop/src/main/java/com/jrsmiffy/examples/solidoop/SolidOopApplication.java
@@ -0,0 +1,13 @@
+package com.jrsmiffy.examples.solidoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidOopApplication.class, args);
+	}
+
+}
